fe7ccafed401ef3d061a6cbf80c2ca4eaeba1ab9
Jenna Westendorf <jenna.westendorf@gmail.com>
2023-12-21 16:37:41 -0800

    feat: add AI api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateStory(event) {
-  event.preventDefault();
-
+function displayStory(response) {
+  //console.log(response.data.answer);
+  console.log('displaying story...');
   new Typewriter('#story', {
-    strings: 'Hello World',
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: '',
   });
+}
+
+function generateStory(event) {
+  event.preventDefault();
+  console.log('generating story...');
+
+  let storyInput = document.querySelector('.story-instructions');
+
+  let apiKey = '1adc8btb0637f4ff3663a3o5a9930eea';
+  let prompt = `User instructions: Please tell a short story about ${storyInput.value}`;
+  let context = `You are a world renowned children's storyteller. Provide only a short 1-2 paragraph story with a good moral in basic HTML. Please separate each line with a <\n>. Make sure to follow the user instructions.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayStory);
 }
 
 let storyFormElement = document.querySelector('#story-generator');
