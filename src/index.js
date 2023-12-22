52b6ca56f90fc488ca373135480582c6c5983da2
Jenna Westendorf <jenna.westendorf@gmail.com>
2023-12-21 21:03:15 -0800

    feat: add text-to-speech

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+function handleAudio(response) {
+  let audio = false;
+  let utterance;
+
+  return function (event) {
+    event.preventDefault();
+
+    if (!audio) {
+      console.log;
+      utterance = new SpeechSynthesisUtterance();
+      utterance.text = response.data.answer;
+      utterance.voice = window.speechSynthesis.getVoices()[0];
+      window.speechSynthesis.speak(utterance);
+      audio = true;
+    } else {
+      if (window.speechSynthesis.paused) {
+        window.speechSynthesis.resume();
+      } else {
+        window.speechSynthesis.pause();
+      }
+    }
+  };
+}
+
 function displayStory(response) {
-  //console.log(response.data.answer);
   console.log('displaying story...');
-  new Typewriter('#story', {
-    strings: response.data.answer,
+  let onRemoveNode = function () {
+    typewriter.stop();
+  };
+  let typewriter = new Typewriter('#story', {
+    strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: '',
+    onRemoveNode: onRemoveNode,
   });
+
+  let readButton = document.querySelector('#read-button');
+  readButton.classList.remove('hidden');
+  readButton.addEventListener('click', handleAudio(response));
 }
 
 function generateStory(event) {
@@ -16,15 +47,17 @@ function generateStory(event) {
   let storyInput = document.querySelector('.story-instructions');
 
   let apiKey = '1adc8btb0637f4ff3663a3o5a9930eea';
-  let prompt = `User instructions: Please tell a short story about ${storyInput.value}`;
-  let context = `You are a world renowned children's storyteller. Provide only a short 1-2 paragraph story with a good moral in basic HTML. Please separate each line with a <\n>. Make sure to follow the user instructions.`;
+  let prompt = `User instructions: Please tell a 1-2 paragraph short story about ${storyInput.value}`;
+  let context = `You are a world renowned children's storyteller. Provide only a short 1-2 paragraph story with a good moral in basic HTML. Due not include a title. Make sure to follow the user instructions.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let storyElement = document.querySelector('#story');
-  storyElement.classList.remove('hidden');
+  document.querySelector('.story').classList.remove('hidden');
   storyElement.innerHTML = `<div class='generating'>Generating a short story about ${storyInput.value}<div class='snippet' data-title='dot-flashing'><div class='stage'><div class='dot-flashing'></div></div></div></div>`;
 
   axios.get(apiUrl).then(displayStory);
+
+  event.target.reset();
 }
 
 let storyFormElement = document.querySelector('#story-generator');
