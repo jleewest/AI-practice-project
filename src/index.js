fb955eb810bc8544e04f2c8d98b4b89d5b4f07dd
Jenna Westendorf <jenna.westendorf@gmail.com>
2023-12-21 17:09:51 -0800

    feat: add loading display

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ function generateStory(event) {
   let context = `You are a world renowned children's storyteller. Provide only a short 1-2 paragraph story with a good moral in basic HTML. Please separate each line with a <\n>. Make sure to follow the user instructions.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let storyElement = document.querySelector('#story');
+  storyElement.classList.remove('hidden');
+  storyElement.innerHTML = `<div class='generating'>Generating a short story about ${storyInput.value}<div class='snippet' data-title='dot-flashing'><div class='stage'><div class='dot-flashing'></div></div></div></div>`;
+
   axios.get(apiUrl).then(displayStory);
 }
 
